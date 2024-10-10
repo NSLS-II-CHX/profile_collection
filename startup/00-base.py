e580cfd52266bfb39aa34c28de5729515f5df50a
xf11id operator <xf11id@xf11id.nsls2.local>
2024-10-10 09:35:06 -0400

    MNT: removed references to persistent dict

--- startup/00-base.py
+++ startup/00-base.py
@@ -14,62 +14,12 @@ nslsii.configure_base(
 # from databroker.v1 import Broker
 
 # c = from_profile("chx-secure")
-
 # db = Broker(c)
 
 # set plot properties for 4k monitors
 plt.rcParams['figure.dpi']=200
 
 from pathlib import Path
-
-import appdirs
-
-
-try:
-    from bluesky.utils import PersistentDict
-except ImportError:
-    import msgpack
-    import msgpack_numpy
-    import zict
-
-    class PersistentDict(zict.Func):
-        def __init__(self, directory):
-            self._directory = directory
-            self._file = zict.File(directory)
-            super().__init__(self._dump, self._load, self._file)
-
-        @property
-        def directory(self):
-            return self._directory
-
-        def __repr__(self):
-            return f"<{self.__class__.__name__} {dict(self)!r}>"
-
-        @staticmethod
-        def _dump(obj):
-            "Encode as msgpack using numpy-aware encoder."
-            # See https://github.com/msgpack/msgpack-python#string-and-binary-type
-            # for more on use_bin_type.
-            return msgpack.packb(
-                obj,
-                default=msgpack_numpy.encode,
-                use_bin_type=True)
-
-        @staticmethod
-        def _load(file):
-            return msgpack.unpackb(
-                file,
-                object_hook=msgpack_numpy.decode,
-                raw=False)
-
-runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
-
-# PersistentDict will create the directory if it does not exist
-metadata = PersistentDict(runengine_metadata_dir)
-# metadata.drop("user_group")
-
-#RE.md.update(dict(RedisJSONDict(redis.Redis("info.chx.nsls2.bnl.gov"), prefix="")))
-#RE.md.pop("user_group")
 
 RE.md = RedisJSONDict(redis.Redis("info.chx.nsls2.bnl.gov"), prefix="")
 
