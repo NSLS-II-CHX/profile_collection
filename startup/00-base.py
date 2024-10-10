9f273ee39ef533f172ab585b564ac26ca993e3ed
xf11id operator <xf11id@xf11id.nsls2.local>
2024-10-10 11:00:00 -0400

    Cleaned 00-base.py

--- startup/00-base.py
+++ startup/00-base.py
@@ -4,6 +4,7 @@ import redis
 from bluesky import RunEngine
 from pathlib import Path
 from redis_json_dict import RedisJSONDict
+from tiled.client import from_profile
 
 nslsii.configure_base(
     get_ipython().user_ns,
@@ -11,23 +12,11 @@ nslsii.configure_base(
     publish_documents_with_kafka=True
 )
 
-# from tiled.client import from_profile
-# from databroker.v1 import Broker
-
-# c = from_profile("chx-secure")
-# db = Broker(c)
-
 # set plot properties for 4k monitors
 plt.rcParams['figure.dpi']=200
 
+# Set the metadata dictionary
 RE.md = RedisJSONDict(redis.Redis("info.chx.nsls2.bnl.gov"), prefix="")
-
-# send ophyd debug log to the console
-# import logging
-#logging.getLogger('ophyd').setLevel('DEBUG')
-#console_handler = logging.StreamHandler()
-#console_handler.setLevel("DEBUG")
-#logging.getLogger('ophyd').addHandler(console_handler)
 
 from ophyd.signal import EpicsSignalBase
 EpicsSignalBase.set_defaults(timeout=60, connection_timeout=60)  # new style
