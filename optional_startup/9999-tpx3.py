86eb29f8ab0832081d094fc32db752ada8ded662
CHX Operator <xf11id@xf11id-ws1.nsls2.bnl.local>
2026-05-18 17:32:14 -0400

    fixing timepix ophyd object and redis changes

--- optional_startup/9999-tpx3.py
+++ optional_startup/9999-tpx3.py
@@ -17,8 +17,6 @@ import bluesky.plan_stubs as bps
 
 from nslsii.ad33 import SingleTriggerV33
 
-temp_assets_path = '/media/nvme/pass-316251/assets/'
-
 class PreciseDtypeSignal(Signal):
     def describe(self):
         ret = super().describe()
@@ -72,7 +70,7 @@ class Tpx3Files(Device):
         # TODO also do the images
         
         self._res_uid = res_uid = new_short_uid()
-        write_path_template = "file:" + temp_assets_path + "timepix-1/%Y/%m/%d/"
+        write_path_template = "file:" + assets_path() + "timepix-1/%Y/%m/%d/"
         self._write_path = write_path = datetime.now().strftime(write_path_template)
         self.raw_filepath.set(write_path).wait()
 
@@ -81,6 +79,7 @@ class Tpx3Files(Device):
 
         # because we need to flush setting to actual server from IOC
         self.raw_write_enable.set(1).wait()
+        self.set_settings.set(1).wait()
         self.set_settings.set(1).wait()
 
         # fill in first guess, this ill increment self._n but we really do not want to!
@@ -104,7 +103,7 @@ class Tpx3Files(Device):
         # because we need to flush setting to actual server from IOC
         self.set_settings.set(1).wait()
 
-        filenames = [f'{self._write_path}/{self._res_uid}/{self._res_uid}_{self._n:05d}_{j:06d}.tpx3' for j in range(self.parent.cam.num_images.get())]
+        filenames = [f'{self._write_path}{self._res_uid}_{self._n:05d}_{j:06d}.tpx3' for j in range(self.parent.cam.num_images.get())]
         self._n += 1
         self.raw_filepaths.set(filenames).wait()
 
@@ -113,7 +112,8 @@ class Tpx3Files(Device):
         self.raw_filepath.set('file:/media/nvme/raw/').wait()
         self.raw_file_template.set(f"garbage").wait()
         self.raw_write_enable.set(0).wait()
-
+        # caput("XF:11ID1-ES{TPX:1}cam1:WriteRaw", "0")
+        # time.sleep(0.5)
         # because we need to flush setting to actual server from IOC
         self.set_settings.set(1).wait()
         return super().unstage()
@@ -128,9 +128,8 @@ class Tpx3HDF(Device):
     
     def stage(self):
          self.hdf5_create_directory.set(-4)
-         write_path_template = 'file:/' + temp_assets_path + "timepix-1/%Y/%m/%d/"
+         write_path_template = assets_path() + "timepix-1/%Y/%m/%d/"
          write_path = datetime.now().strftime(write_path_template)
-         print(write_path)
          self.hdf5_file_path.put(write_path)
 
 class TimePixDetector(SingleTriggerV33, AreaDetector):
@@ -140,6 +139,8 @@ class TimePixDetector(SingleTriggerV33, AreaDetector):
     hdf_plugin = Cpt(Tpx3HDF, "HDF1:")
 
     files = Cpt(Tpx3Files, "cam1:")
+    
+
 
     stats1 = Cpt(StatsPlugin_V34, "Stats1:")
     stats2 = Cpt(StatsPlugin_V34, "Stats2:")
@@ -158,7 +159,7 @@ class TimePixDetector(SingleTriggerV33, AreaDetector):
     
     # def stage(self):
     #     self.hdf5_create_directory.set(-4).wait()
-    #     write_path_template = temp_assets_path + "timepix-1/%Y/%m/%d/"
+    #     write_path_template = assets_path() + "timepix-1/%Y/%m/%d/"
     #     write_path = datetime.now().strftime(write_path_template)
     #     self.hdf5_file_path.set(write_path).wait()
     #     self.files.stage()
@@ -184,10 +185,10 @@ class TimePixDetector(SingleTriggerV33, AreaDetector):
 
         yield from bps.mv(self.cam.acquire_period, period)
         yield from bps.mv(self.cam.acquire_time, real_exp)
-        yield from self.set_num_images(num_frames)            
+        yield from self.set_num_images(num_frames)      
 
 
-tpx3 = TimePixDetector("TPX3-TEST:", name="tpx3")
+tpx3 = TimePixDetector("XF:11ID1-ES{TPX:1}", name="tpx3")
 print("Reloaded tpx3!")
 
 for j in range(1, 5):
--- startup/00-base.py
+++ startup/00-base.py
@@ -44,7 +44,7 @@ nslsii.configure_base(get_ipython().user_ns,
                       redis_url="xf11id1-chx-redis1.nsls2.bnl.gov",
                       redis_port=6380,
                       redis_ssl=True,
-                      publish_documents_with_kafka=False)
+                      publish_documents_with_kafka=True)
 
 print("Initializing Tiled reading client...\nMake sure you check for duo push.")
 tiled_reading_client = from_profile("nsls2", username=None, include_data_sources=True)["chx"]["raw"]
